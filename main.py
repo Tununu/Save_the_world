cf158b6726b72cbc606ecd5e3cff1faaa66514be
Tununu <125532801+Tununu@users.noreply.github.com>
2023-03-20 12:05:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    data = None
     name = request.form.get('name')
     email = request.form.get('email')
     age = request.form.get('age')
@@ -16,16 +17,19 @@ def submit():
     class_ = request.form.get('class')
     rating = request.form.get('rating')
     feedback = request.form.get('feedback')
+
+    if(feedback == 'Easter egg'):
+		    data = 'egg'
+  
     with open('submission.csv', 'a', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow([name, email, age, gender, class_, rating, feedback])
-    return redirect(url_for('success', name=name))
+    return redirect(url_for('success', name=name, data=data))
     csvfile.close()
   
-@app.route('/success/<name>')
-def success(name):
-    return render_template('success.html', name=name)
+@app.route('/success/<name>, <data>')
+def success(name, data):
+    return render_template('success.html', name=name, data=data)
 
 
 app.run(host='0.0.0.0', port=81)
-    
